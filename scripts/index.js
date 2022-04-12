e174bf987ecf9059f6b443fc8154679d8ce392a8
Daria Khanina <khaninadaria@gmail.com>
2022-04-12 15:59:02 +0300

    fix: удалить функциональность не из задания

--- scripts/index.js
+++ scripts/index.js
@@ -4,25 +4,15 @@ const closeButton = popupElement.querySelector('.popup__close-button');
 
 function openPopup() {
   popupElement.classList.add('popup_opened');
-  document.addEventListener('keyup', onDocumentKeyUp);
 }
  
-
-
 function closePopup() {
   popupElement.classList.remove('popup_opened');
-  document.removeEventListener('keyup', onDocumentKeyUp)
-}
-function onDocumentKeyUp(event){
-    if (event.key === ESC_KEY) {
-        closePopup();
-    }
 }
 
 editButton.addEventListener('click', openPopup);
   
 closeButton.addEventListener('click', closePopup);
-const ESC_KEY = "Escape";
 
 const profileName = document.querySelector('.profile__title');
 const profileDescription = document.querySelector('.profile__subtitle');
