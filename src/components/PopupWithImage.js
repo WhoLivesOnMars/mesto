a5629776772ac41303a70991f66f146ca6b68525
Daria Khanina <khaninadaria@gmail.com>
2023-03-08 22:56:43 +0300

    hotfix: elements searching has been moved to the constructor

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,22 +3,14 @@ import Popup from './Popup.js';
 export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
-    // this._image = document.querySelector('.elements__item');
-    // this._title = document.querySelector('.card__title');
+    this._image = this._popup.querySelector('.picture-viewer__image');
+    this._title = this._popup.querySelector('.popup__caption');
   };
 
   open({ name, link }) {  
-    const popupPic = this._popup.querySelector('.picture-viewer__image');
-    const popupCaption = this._popup.querySelector('.popup__caption');
-    popupPic.src = link;
-    popupPic.alt = name;
-    popupCaption.textContent = name;
-
-    /*
-     this._image.src = link;
-     this._image.alt = name;
-     this._title.textContent = name;
-    */
+    this._image.src = link;
+    this._image.alt = name;
+    this._title.textContent = name;
 
     super.open();
   };
