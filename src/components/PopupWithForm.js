98ca020fefe37e6ffcae05c5967f7d24e6dd74b8
Daria Khanina <khaninadaria@gmail.com>
2023-03-08 20:50:52 +0300

    hotfix: changed searching for input list

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -5,10 +5,10 @@ export default class PopupWithForm extends Popup {
     super(popupSelector);
     this._handlerSubmitForm = handlerSubmitForm;
     this._formSelector = document.querySelector(popupSelector);
+    this._inputList = this._formSelector.querySelectorAll('.popup__input');
   };
 
   _getInputValues() {
-    this._inputList = this._formSelector.querySelectorAll('.popup__input');
     this._formValues = {};
     this._inputList.forEach(input => this._formValues[input.name] = input.value);
     return this._formValues;
